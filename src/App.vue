7ad4157d11e19abfa1dc760f497eb0db41de9b79
Nick Hulea <naeluh@gmail.com>
2018-05-18 16:36:38 -0700

    detail view component framed

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app" class="Prose">
     <div v-if="this.$store.getters.loading" id="load-component"><div class="parent"><div class="child">Loading ...</div></div></div>
   	<Nav v-if="!this.$store.getters.loading" v-cloak></Nav>
-    <router-view v-if="!this.$store.getters.loading" :arr="this.$store.state.site" v-cloak></router-view>
+    <main>
+      <router-view v-if="!this.$store.getters.loading" :arr="this.$store.state.site" v-cloak></router-view>
+    </main>
   </div>
 </template>
 
@@ -18,22 +20,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
